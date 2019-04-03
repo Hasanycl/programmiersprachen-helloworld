9c2ae69e27d5521782c117c7ee459397d06c06c5
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2019-04-03 17:59:15 +0200

    implement dummy for fract function

---
+++ source/fract.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+float fract(float in) {
+
+  std::cout << "Fract not implemented yet" << "\n";
+  return 0.0;
+}
+
+int main() {
+  std::cout << "Fract of 5.31 is : " << fract(5.3) << std::endl;
+  return 0;
+}
